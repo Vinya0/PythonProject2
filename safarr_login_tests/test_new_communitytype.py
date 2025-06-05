582cc9db88cfe1d8146492d544199f83abb934c1
Vinya <vinyaviswanathan0410@gmail.com>
2025-06-05 12:53:17 +0530

    initial

--- safarr_login_tests/test_new_communitytype.py
+++ safarr_login_tests/test_new_communitytype.py
@@ -11,89 +11,135 @@ from safarr_setup.config import config
 from constant import validation_assert
 from constant import input_field
 
+chrome_options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+driver.maximize_window()
+wait = WebDriverWait(driver, 25)
+
+# Functions
+
+def refresh_page_and_wait():
+    driver.get(config.WEB_URL)
+    time.sleep(2)
+
+def email_input_field():
+    return wait.until(EC.presence_of_element_located((By.NAME, "email")))
+
+def password_input_field():
+    return wait.until(EC.presence_of_element_located((By.NAME, "password")))
+
+def login_button():
+    return wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@type='submit']")))
+
+def community():
+    return wait.until(EC.element_to_be_clickable((By.XPATH, "//span[contains(text(), 'Communities')]")))
+
+def community_type():
+    return wait.until(EC.element_to_be_clickable((By.XPATH, "//span[normalize-space()='Community Type']")))
+
+def search_input():
+    return wait.until(EC.presence_of_element_located((By.XPATH, "//input[@placeholder='Search by Community Type']")))
+
+def toggle_for_name(name):
+    return wait.until(EC.element_to_be_clickable((By.XPATH, "//tbody/tr[1]/td[3]/div[1]/span[1]/label[1]/div[1]")))
+
+def confirm_button():
+    return wait.until(EC.element_to_be_clickable((By.XPATH, "//button[normalize-space()='Confirm']")))
+
+def cancel_button():
+    return wait.until(EC.element_to_be_clickable((By.XPATH, "//button[normalize-space()='Cancel']")))
+
+def pagination():
+    return wait.until(EC.element_to_be_clickable((By.XPATH,"//button[@aria-label='Click Next']")))
+
+def click_edit_icon():
+    return wait.until(EC.element_to_be_clickable(
+        (By.XPATH, "//*[@id='root']//table/tbody/tr[1]/td[3]//button")))
+
+def open_language_dropdown():
+    return wait.until(EC.element_to_be_clickable(
+        (By.XPATH, "//div[contains(@id, 'react-select-') and contains(@id, '-placeholder')]")))
+
+def select_language_option():
+    return wait.until(EC.element_to_be_clickable(
+        (By.XPATH, "//*[contains(@id, 'react-select-') and contains(@id, '-option-0')]")))
+
+def language_error():
+    return wait.until(EC.visibility_of_element_located(
+        (By.XPATH, "//p[contains(text(), 'Language is required')]")))
+
+
+
+def click_submit_button():
+    return wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@type='submit']")))
+
+def click_close_button():
+    return wait.until(EC.element_to_be_clickable(
+        (By.XPATH, "//button[@type='reset']")))
+
+def edit_community_name():
+    return wait.until(EC.element_to_be_clickable(
+        (By.XPATH, "//button[@type='reset']")))
+
+
+
+# Test Class
 
 class TestCommunityType:
 
-    def setup_class(self):
-        chrome_options = webdriver.ChromeOptions()
-        service = ChromeService(ChromeDriverManager().install())
-        self.driver = webdriver.Chrome(service=service, options=chrome_options)
-        self.driver.maximize_window()
-        self.wait = WebDriverWait(self.driver, 25)
-
-    def refresh_page_and_wait(self):
-        self.driver.get(config.WEB_URL)
-        time.sleep(2)
-
-    def email_input_field(self):
-        return self.wait.until(EC.presence_of_element_located((By.NAME, "email")))
-
-    def password_input_field(self):
-        return self.wait.until(EC.presence_of_element_located((By.NAME, "password")))
-
-    def login_button(self):
-        return self.wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@type='submit']")))
-
-    def community(self):
-        return self.wait.until(EC.element_to_be_clickable((By.XPATH,"//span[contains(text(), 'Communities')]")))
-
-    def community_type(self):
-        return self.wait.until(EC.element_to_be_clickable((By.XPATH,"//span[normalize-space()='Community Type']")))
-
-    def search_input(self):
-        return self.wait.until(
-            EC.presence_of_element_located((By.XPATH, "//input[@placeholder='Search by Community Type']")))
-
-    def toggle_for_name(self, name):
-        return self.wait.until(EC.element_to_be_clickable((
-            By.XPATH,
-            "//tbody/tr[1]/td[3]/div[1]/span[1]/label[1]/div[1]")))
-
-    def confirm_button(self):
-        return self.wait.until(EC.element_to_be_clickable((
-            By.XPATH,
-            "//button[normalize-space()='Confirm']")))
-
-    def cancel_button(self):
-        return self.wait.until(EC.element_to_be_clickable((
-            By.XPATH,
-            "//button[normalize-space()='Cancel']")))
-
     def test_valid_email_valid_password(self):
-        self.refresh_page_and_wait()
-        self.email_input_field().send_keys(config.CORRECT_EMAIL)
-        self.password_input_field().send_keys(config.CORRECT_PASSWORD)
-        self.login_button().click()
-        self.wait.until(EC.url_contains("/users"))
-        assert "/users" in self.driver.current_url
+        refresh_page_and_wait()
+        email_input_field().send_keys(config.CORRECT_EMAIL)
+        password_input_field().send_keys(config.CORRECT_PASSWORD)
+        login_button().click()
+        wait.until(EC.url_contains("/users"))
+        assert "/users" in driver.current_url
 
     def test_navigate_to_community_type(self):
-        # Assume user already logged in from previous test or call login steps here
-        self.refresh_page_and_wait()
-        # Click on "Communities" menu
-        self.community().click()
-        time.sleep(1)  # small wait to let submenu appear
-        # Click on "Community Type" submenu
-        self.community_type().click()
-        time.sleep(2)  # wait for Community Type page to load
-        # Validate we reached the Community Type page URL or some element unique to that page
-        assert "/community-type" in self.driver.current_url
+        refresh_page_and_wait()
+        community().click()
+        time.sleep(1)
+        community_type().click()
+        time.sleep(2)
+        assert "/community-type" in driver.current_url
+
+    def test_pagination_next_button(self):
+        for _ in range(5):  # number of times to click
+            pagination().click()
+            time.sleep(2)
 
     def test_search(self):
-        self.search_input().send_keys(input_field.COMMUNITY_NAME[0])
+        search_input().send_keys(input_field.COMMUNITY_NAME[0])
         time.sleep(2)
 
-    def test_toggle(self):
-        self.toggle_for_name(input_field.COMMUNITY_NAME[0]).click()
-        time.sleep(1)
-        self.confirm_button().click()
 
-        wait = WebDriverWait(self.driver, 10)
-        toast = wait.until(EC.visibility_of_element_located(
-            (By.CLASS_NAME, "notification-title")
-        ))
+    def test_toggle(self):
+        toggle_for_name(input_field.COMMUNITY_NAME[0]).click()
+        time.sleep(1)
+        confirm_button().click()
+
+        toast = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "notification-title")))
         print("Toast message:", toast.text)
         assert "Community Type has been updated successfully" in toast.text
+        time.sleep(1)
+
+
+    # def test_edit_translation(self):
+    #         click_edit_icon().click()
+    #         open_language_dropdown().click()
+    #         select_language_option().click()
+    #         click_submit_button().click()
+    #         time.sleep(2)  # Optional: wait for redirection
+    #         assert "/edit-community-type" in driver.current_url
+    #         time.sleep(2)
+
+    def test_edit_translation(self):
+        click_edit_icon().click()
+        open_language_dropdown().click()
+        # select_language_option().click()  # left unclicked intentionally
+        click_submit_button().click()
+        time.sleep(2)
+        assert self.language_error().text == validation_assert.LANGUAGE
 
 
 
